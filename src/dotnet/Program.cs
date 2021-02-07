cd8943b442a53f40ac358e265fbcc391799925b8
LemonU <lassaegsi@gmail.com>
2021-02-07 13:29:59 -0500

    implemented merge sort w/ tests

---
+++ src/dotnet/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using Tests;
+using Sorts;
+
+class Program
+{
+    static void TimeTest()
+    {
+        string outfileName = "out.csv";
+        int nLimit = 500 * 1000;
+        List<int> nCol = new List<int>();
+        List<long> tMerge = new List<long>();
+
+        StreamWriter sw = new StreamWriter(outfileName);
+        Stopwatch timer = new Stopwatch();
+
+        Console.WriteLine("Starting timing test...");
+        for (int n = 1; n <= nLimit; n+=100)
+        {
+            int[] arr = Helper.CreateRandomIntArray(n);
+            int[] cpy = new int[n];
+            arr.CopyTo(cpy, 0);
+            Console.WriteLine($"n={n}");
+
+            timer.Restart();
+            Sort.MergeSort(arr);
+            timer.Stop();
+            long deltaT1 = timer.ElapsedMilliseconds;
+
+            // timer.Restart();
+            // Array.Sort(cpy);
+            // timer.Stop();
+            // long deltaT2 = timer.ElapsedMilliseconds;
+            sw.WriteLineAsync(string.Format("{0:G},{1:G}", n, deltaT1));
+        }
+        // Console.WriteLine("Testing complete, outputting to file...");
+        // string[] rows = new string[nLimit];
+        // int[] nCols = nCol.ToArray();
+        // for (int i = 0; i < nLimit; i++)
+        //     rows[i] = string.Format("{0:G},{1:G}", nCols[i], tMerges[i]);
+        // foreach (var s in rows)
+        //     sw.WriteLine(s);
+        sw.Close();
+    }
+    static void Main()
+    {
+        // int n = 50;
+        // int[] arr = Helper.CreateRandomIntArray(n);
+        // int[] copy = new int[n];
+        // arr.CopyTo(copy, 0);
+
+        // Helper.PrintArray(arr);
+        // Helper.PrintArray(copy);
+
+        // Sort.MergeSort(arr);
+        // Array.Sort(copy);
+
+        // Helper.PrintArray(arr);
+        // Console.WriteLine(Test.TestArrayEquals(copy, arr));
+
+        TimeTest();
+
+        // var watch = new System.Diagnostics.Stopwatch();
+        // int[] arr = Helper.CreateRandomIntArray(9000*1000);
+            
+        // watch.Start();
+        // Sort.MergeSort(arr);
+        // watch.Stop();
+        // Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+
+        // watch.Start();
+        // Array.Sort(arr);
+        // watch.Stop();
+        // Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+}
+
--- src/dotnet/Sorts.cs
+++ src/dotnet/Sorts.cs
@@ -1,19 +1,78 @@
-public class Sorts
+using System;
+using Tests;
+
+namespace Sorts
 {
-    public static void QuickSort<T>(T[] arr) where T : System.IComparable
+    class Sort
     {
+        public static void QuickSort<T>(T[] arr) where T : IComparable
+        {
 
+        }
+
+        public static void MergeSort<T>(T[] arr) where T : IComparable
+        {
+            MergeSort(arr, 0, arr.Length-1);
+        }
+
+        private static void MergeSort<T>(T[] arr, int lo, int hi) where T : IComparable
+        {
+            // Console.WriteLine("lo={0} hi={1}", lo, hi);
+            // Console.ReadLine();
+            if (hi - lo <= 0)
+                return;
+
+            int mid = (hi - lo) / 2 + lo;
+            MergeSort(arr, lo, mid);
+            MergeSort(arr, mid+1, hi);
+            Merge(arr, lo, mid, hi);
+        }
+
+        private static void Merge<T>(T[] arr, int lo, int mid, int hi) where T : IComparable
+        {
+            T[] merged = new T[hi - lo + 1];
+            int sub1Ind = lo;
+            int sub2Ind = mid+1;
+            int mergedInd = 0;
+            while (sub1Ind <= mid && sub2Ind <= hi)
+            {
+                if (Less(arr[sub1Ind], arr[sub2Ind]))
+                    merged[mergedInd++] = arr[sub1Ind++];
+                else
+                    merged[mergedInd++] = arr[sub2Ind++];
+            }
+            if (sub1Ind <= mid)
+                for (;sub1Ind <= mid; sub1Ind++, mergedInd++)
+                    merged[mergedInd] = arr[sub1Ind];
+            else
+                for (;sub2Ind <= hi; sub2Ind++, mergedInd++)
+                    merged[mergedInd] = arr[sub2Ind];
+
+            for (int i = 0; i < merged.Length; i++)
+                arr[lo+i] = merged[i];
+        }
+
+        public static void HeapSort()
+        {
+
+        }
+
+        private static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private static bool Greater<T>(T lhs, T rhs) where T : IComparable
+        {
+            return lhs.CompareTo(rhs) > 0;
+        }
+
+        private static bool Less<T>(T lhs, T rhs) where T : IComparable
+        {
+            return lhs.CompareTo(rhs) < 0;
+        }
     }
-
-    public static void MergeSort() {
-
-    }
-
-    public static void HeapSort() {
-
-    }
-
-    public static void Main(String[] args) {
-
-    }
+    
 }
