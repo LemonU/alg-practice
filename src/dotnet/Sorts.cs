2eb047cf94cff8a04bca473ad1f2d0a2978c0303
LemonU <lassaegsi@gmail.com>
2021-02-06 21:24:46 -0500

    added stubs for C# implementations

---
+++ src/dotnet/Sorts.cs
@@ -0,0 +1,19 @@
+public class Sorts
+{
+    public static void QuickSort<T>(T[] arr) where T : System.IComparable
+    {
+
+    }
+
+    public static void MergeSort() {
+
+    }
+
+    public static void HeapSort() {
+
+    }
+
+    public static void Main(String[] args) {
+
+    }
+}
